3f5e814f031df57cf9eb4b22d8b0b7084d654a5d
filipecosta90 <filipecosta.90@gmail.com>
2016-06-17 23:09:48 +0100

    working on front-end EVENT and USER authentication

---
+++ li4_backend/Controllers/EventoController.cs
@@ -0,0 +1,120 @@
+﻿using li4_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace li4_backend.Controllers
+{
+    public class EventoController : Controller
+    {
+        li4_back_end_entities base_dados = new li4_back_end_entities();
+
+        // GET: Evento
+        public ActionResult Index()
+        {
+            return View(base_dados.Eventoes.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Criar(Evento novo_evento)
+        {
+            if (ModelState.IsValid)
+            {
+                base_dados.Eventoes.Add(novo_evento);
+                base_dados.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(novo_evento);
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            Evento evento = base_dados.Eventoes.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evento);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                base_dados.Entry(evento).State = System.Data.Entity.EntityState.Modified;
+                base_dados.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(evento);
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            Evento evento = base_dados.Eventoes.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evento);
+        }
+
+        [HttpGet]
+        public ActionResult Detalhe(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            Evento evento = base_dados.Eventoes.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evento);
+        }
+
+        [HttpPost]
+        public ActionResult Detalhe(Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(evento);
+        }
+
+
+        [HttpPost]
+        public ActionResult Eliminar(int? id, Evento evento1)
+        {
+            Evento evento = new Evento();
+            if (ModelState.IsValid)
+            {
+                evento = base_dados.Eventoes.Find(id);
+                base_dados.Entry(evento).State = System.Data.Entity.EntityState.Deleted;
+                base_dados.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(evento);
+        }
+    }
+}
--- li4_backend/Controllers/HomeController.cs
+++ li4_backend/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using li4_backend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +9,48 @@ namespace li4_backend.Controllers
 {
     public class HomeController : Controller
     {
+
+        li4_back_end_entities base_dados = new li4_back_end_entities();
+        Utilizador user_actual;
         public ActionResult Index()
+        {
+            user_actual = new Utilizador();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index( Utilizador user_corrente)
+        {
+            if (ModelState.IsValid)
+            {
+                Utilizador user_bd = base_dados.Utilizadors.Find(user_corrente.id_utlizador);
+                if (user_bd != null) { 
+                if (user_corrente.tipo == "admin" && user_bd.tipo == "admin" && user_corrente.password == user_bd.password )
+                {
+                    return RedirectToAction("IndexAdmin" , user_corrente);
+                }
+                else 
+                {
+                        if (user_corrente.password == user_bd.password ) { 
+                    return RedirectToAction("IndexUser", user_corrente);
+                }
+                    }
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult IndexAdmin(Utilizador user_corrente)
         {
             return View();
         }
 
-        public ActionResult Utilizadores() {
+        public ActionResult IndexUser(Utilizador user_corrente)
+        {
             return View();
         }
+
+
 
     }
 }
